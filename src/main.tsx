257344b09be958d04cd5c4607d9cc2004132cc23
Pablo Alencar <pabloalencarlucas7@gmail.com>
2024-03-20 16:37:52 -0300

    add project routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./pages/App/index.tsx";
 import { Reset } from "./styles/reset.ts";
 import { ThemeProvider } from "styled-components";
 import { appTheme } from "./styles/theme.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AppRoutes } from "./routes/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={new QueryClient()}>
     <ThemeProvider theme={appTheme}>
-    <App />
+    <AppRoutes />
     <Reset />
     </ThemeProvider>
     </QueryClientProvider>
