3b01f2b2469092088841308d4968724b0459de1e
Pablo Alencar <pabloalencarlucas7@gmail.com>
2024-03-21 16:37:59 -0300

    add query and card pokemon

--- src/hooks/useQueryPokemonPage.ts
+++ src/hooks/useQueryPokemonPage.ts
@@ -1,16 +1,43 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query";
+import { API } from "../configs/api";
+import { useState } from "react";
+import { Pokemon } from "../@types/pokemon";
 
 export function useQueryPokemonPage() {
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(30);
+  const [totalPages, setTotalPages] = useState(0);
 
-    function getPokemonPage() {
-        return "Get Pokemon Page";
-    }
+  async function getPokemonPage(page = 1, limit = 30) {
+    if (page <= 0) page = 1;
+    const offset = (page - 1) * limit;
 
-    const query = useQuery({
-        queryKey: [`getPokemonPage`],
-        queryFn: () => getPokemonPage(),
+    const { data } = await API.get(`/pokemon?limit=${limit}&offset=${offset}`);
+
+    const pokemonPromise = data.results.map(async (pokemon: {url: string}) => {
+      const response = await fetch(pokemon.url);
+      const data = await response.json();
+      return data;
     });
-    return {
-        ...query,
-    }
+
+    const pokemonData = await Promise.all(pokemonPromise);
+
+    const totalPokemon = data.count;
+    const totalPagesAPI = Math.ceil(totalPokemon / limit);
+
+    if (totalPages != totalPagesAPI) setTotalPages(totalPagesAPI);
+
+    return pokemonData as Pokemon[];
+  }
+
+  const query = useQuery({
+    queryKey: [`getPokemonPage${page}-${limit}`, page, limit],
+    queryFn: () => getPokemonPage({ page, limit }),
+  });
+
+  return {
+    ...query,
+    page,
+    totalPages,
+  };
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,35 @@
+import { Link } from "react-router-dom";
 import { useQueryPokemonPage } from "../../hooks/useQueryPokemonPage";
 import { Container } from "./style";
+import { PokemonCard } from "../../components/PokemonCard";
 
 export function Home() {
-  const { data } = useQueryPokemonPage();
+  const { data, isLoading, error, page, totalPages } = useQueryPokemonPage();
   console.log(data);
 
   return (
     <Container>
-      <h1>Home</h1>
-      <p>{data}</p>
+      <h1>{"Bem-vindo(a) à Pokédex"}</h1>
+      {isLoading && <span className="loading">Loading...</span>}
+      {!isLoading && error && <span className="loading">Error...</span>}
+
+      <div className="gridCards">
+        {data?.map((pokemon) => {
+          return (
+            <Link to={"/details"} key={pokemon.id}>
+              <PokemonCard pokemon={pokemon} />
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className="paginationComponent">
+        <button>&lt; Anterior</button>
+        <span className="numberPage">
+          {page}/{totalPages}
+        </span>
+        <button>Próxima &gt;</button>
+      </div>
     </Container>
   );
 }
