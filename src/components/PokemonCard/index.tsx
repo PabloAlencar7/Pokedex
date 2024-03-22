5910b2e63ed31e1a30e488f32da7f7d7b76188b7
Pablo Alencar <pabloalencarlucas7@gmail.com>
2024-03-22 15:38:50 -0300

    add home card pokemon

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,16 +1,26 @@
-import { Pokemon } from "../../@types/pokemon"
+import { Pokemon } from "../../@types/pokemon";
+import { CardType } from "../CardType";
+import { Container } from "./style";
 
 type Props = {
-    pokemon: Pokemon;
-}
+  pokemon: Pokemon;
+};
 
-export function PokemonCard ({pokemon}: Props) {
-    return (
-        <>
-            <img 
-            src={ pokemon.sprites.other["official-artwork"].front_default} 
-            alt={pokemon.name} />
-            <strong>#{pokemon.id} {pokemon.name}</strong>
-        </>
-    )
+export function PokemonCard({ pokemon }: Props) {
+  return (
+    <Container>
+      <img
+        src={pokemon.sprites.other["official-artwork"].front_default}
+        alt={pokemon.name}
+      />
+      <strong>
+        #{pokemon.id} {pokemon.name}
+      </strong>
+      <div className="boxTypes">
+        {pokemon.types.map((type) => {
+          return <CardType key={type.type.name}  type={type.type.name} />;
+        })}
+      </div>
+    </Container>
+  );
 }
